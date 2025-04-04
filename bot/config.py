b04a446b9c1071d44d5bcfa8500223c713aafdcd
Tracy S Werts <wertstracys@gmail.com>
2025-04-04 08:20:03 -0400

    update config.py

--- bot/config.py
+++ bot/config.py
@@ -48,7 +48,7 @@ class Config:
             self.DYNO = config("DYNO", default=None)
             self.ENCODER = config("ENCODER", default=None)
             self.EXT_CAP = config("EXTENDED_CAPTIONS", default=False, cast=bool)
-            self.FBANNER = config("FBANNER", default=True, cast=bool)
+            self.FBANNER = config("FBANNER", default=False, cast=bool)
             self.FCHANNEL = config("FCHANNEL", default=0, cast=int)
             self.FCHANNEL_STAT = config("FCHANNEL_STAT", default=0, cast=int)
             self.FCODEC = config("FCODEC", default=None)
