ffe2aa4fce7e96d3554eb59931f28057733a76f4
Nubuki-all <121189493+Nubuki-all@users.noreply.github.com>
2025-04-17 05:35:30 +0000

    pyLint: auto-fixes

--- bot/config.py
+++ bot/config.py
@@ -70,7 +70,9 @@ class Config:
             self.LOG_CHANNEL = config("LOG_CHANNEL", default=0, cast=int)
             self.LOGS_IN_CHANNEL = config("LOGS_IN_CHANNEL", default=False, cast=bool)
             self.MI_CAP = config("MI_IN_CAPTION", default=True, cast=bool)
-            self.MUX_ARGS = config("MUX_ARGS", default='-map 0 -c:v copy -c:a libopus -b:a 96k -ac 2')
+            self.MUX_ARGS = config(
+                "MUX_ARGS", default="-map 0 -c:v copy -c:a libopus -b:a 96k -ac 2"
+            )
             self.NO_BANNER = config("NO_BANNER", default=False, cast=bool)
             self.NO_TEMP_PM = config("NO_TEMP_PM", default=False, cast=bool)
             self.OVR = config("OVR", default=None)
